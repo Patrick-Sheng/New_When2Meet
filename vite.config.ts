aea19f79abac715bad049d5025e505f3a86e35bf
Patrick Sheng <sheng.patrick@gmail.com>
2025-10-07 19:22:46 +1300

    fix: reconnected to a new database to fix connection issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://rsagursnppdyiwpivqwy.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzYWd1cnNucHBkeWl3cGl2cXd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NTcxNTMsImV4cCI6MjA3NTEzMzE1M30.SU5zxvBUimaaeLxPpjOeqXEFCz1SHDXhI9Y9OCFUFvE"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // https://vite.dev/config/
 export default defineConfig({
